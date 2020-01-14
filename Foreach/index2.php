716447f9f84faa11b6ba695cee792eee49ac4271
Hor123 <hortensere@aol.com>
2020-01-14 12:54:07 +0100

    Boucles

---
+++ Foreach/index2.php
@@ -0,0 +1,54 @@
+<?php
+
+//Afficher les noms et prénoms des élèves de ce collège
+
+$college = array(
+  'Sixieme' => array(
+    array("Nom" => "Payet", "Prenom" => "Mickael"),
+    array("Nom" => "Hoareau", "Prenom" => "Christine"),
+    array("Nom" => "Maillot", "Prenom" => "Laure"),
+  ),
+  'Cinquieme' => array(
+    array("Nom" => "Bourdon", "Prenom" => "Didier"),
+    array("Nom" => "Legitimus", "Prenom" => "Pascal"),
+    array("Nom" => "Campan", "Prenom" => "Bernard"),
+    array("Nom" => "Fois", "Prenom" => "Marina"),
+    array("Nom" => "Foresti", "Prenom" => "Florence"),
+  ),
+  'Quatrieme' => array(
+    array("Nom" => "Willis", "Prenom" => "Bruce"),
+    array("Nom" => "Lawrence", "Prenom" => "Laurence"),
+    array("Nom" => "Johannson", "Prenom" => "Scarlett"),
+    array("Nom" => "Jackson", "Prenom" => "Samuel"),
+  ),
+);
+
+echo "10.Les élèves du collège : <br>";
+
+//Ajoutez votre code ici
+
+foreach ($college as $classe => $eleve) {
+    echo "<br>"."$classe"." : "."<br>";
+
+    foreach ($eleve as $identite) {
+        foreach ($identite as $nom){
+            echo "$nom"." ";
+        }
+    }
+}
+
+echo "<br><br>";
+
+$nouvelEleve = array(array("Nom"=>"Haddock","Prenom"=>"Capitaine"));
+
+$college["troisieme"]=$nouvelEleve;
+
+
+foreach ($college as $classe => $eleve) {
+    echo "<br>".$classe." : "."<br>";
+    foreach ($eleve as $identite=>$nom) {
+        foreach ($nom as $propriete=>$valeur){
+            echo $propriete." : ".$valeur."<br>";
+        }
+    }
+};
---
+++ Foreach/index3.php
@@ -0,0 +1,79 @@
+<?php
+
+//----------------------------------------
+//Afficher toutes les informations de la vidéothèque
+$videotheque = array(
+    array(
+        'nom' => 'Independance day',
+        'date' => 1996,
+        'realisateur' => 'Roland Emmerich',
+        'acteurs' => array(
+            'Will Smith', 'Bill Pullman', 'Jeff Goldblum', 'Mary McDonnell',
+        ),
+    ),
+    array(
+        'nom' => 'Bienvenue a Gattaca',
+        'date' => 1998,
+        'realisateur' => 'Andrew Niccol',
+        'acteurs' => array(
+            'Ethan Hawke', 'Uma Thurman', 'Jude Law',
+        ),
+    ),
+    array(
+        'nom' => 'Forrest Gump',
+        'date' => 1994,
+        'realisateur' => 'Robert Zemeckis',
+        'acteurs' => array(
+            'Tom Hanks', 'Gary Sinise',
+        ),
+    ),
+    array(
+        'nom' => '12 hommes en colere',
+        'date' => 1957,
+        'realisateur' => 'Sidney Lumet',
+        'acteurs' => array(
+            'Henry Fonda','Martin Balsam','John Fiedler','Lee J. Cobb','E.G. Marshall',
+        ),
+    ),
+);
+
+echo '12.Mes films : <br>';
+//ajoutez votre code ici
+
+/*
+foreach ($videotheque as $films){
+    foreach ($films as $propriete =>$valeur) {
+        echo $propriete. " : " . $valeur . "<br>";
+        if ($propriete == 'acteurs') {
+            foreach ($films['acteurs'] as $acteur => $noms) {
+                echo $noms . "<br>";
+            }
+        }
+    }
+}
+*/
+foreach ($videotheque as $key => $value) {
+    echo "nom : ".$value["nom"]."<br>";
+    echo "date : ".$value["date"]."<br>";
+    echo "realisateur : ".$value["realisateur"]."<br>";
+    echo "acteur : ";
+    foreach ($value["acteurs"] as $acteur){
+        echo $acteur.", ";
+    }
+echo "<br><br>";
+
+
+}
+
+
+echo '<br><br>';
+
+//----------------------------------------
+//Afficher toutes les informations de la vidéothèque
+//reprenez le tableau ci-dessus, ajoutez-y 3 de vos films préférés avec les mêmes
+//informations (nom, date, realisateur, acteurs) et en plus de ces informations
+//rajoutez un synopsis
+
+echo '13.Mes films : <br>';
+//ajoutez votre code ici
+echo '<br><br>';
